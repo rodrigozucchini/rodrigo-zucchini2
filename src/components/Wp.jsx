8c2e0c20231a832cefe22db3b2d794b7b9edaab7
unknown <rodrigozucchini1997@gmail.com>
2024-05-01 02:06:07 -0300

    cambios wpp responsive

--- src/components/Wp.jsx
+++ src/components/Wp.jsx
@@ -5,7 +5,7 @@ const Wp = () => {
   return (
     <div title="¿Necesitas ayuda?">
         <a href="https://wa.link/ue0uv8">
-        <img className="btn-whatsapp" target="_blank" src="https://clientes.dongee.com/whatsapp.png" width="64" height="64" alt="Whatsapp" />
+        <img className="btn-whatsapp" target="_blank" src="https://clientes.dongee.com/whatsapp.png" width="84" height="84" alt="Whatsapp" />
         </a>
     </div>
   )
